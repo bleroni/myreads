65bf23dae31c125f3694806e4a6a14512a940404
Bleron Limani <bleroni@gmail.com>
2021-02-18 10:08:52 +0100

    feat: Added the ability to display books without covers

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,11 +3,15 @@ import propTypes from 'prop-types';
 import SelectComponent from './SelectComponent';
 
 class Book extends Component {
+
     render() {
+
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.props.bookDetails.imageLinks.thumbnail}")` }}></div>
+                    {this.props.bookDetails.imageLinks &&
+                        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.props.bookDetails.imageLinks.thumbnail}")` }}></div>
+                    }
                     <div className="book-shelf-changer">
                         <SelectComponent onChangeShelf={this.props.onChangeShelf} shelves={this.props.shelves} bookDetails={this.props.bookDetails} />
                     </div>
